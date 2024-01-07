a194c417f589ad97ea75d7a4cebf24f9c64c9e90
cahyadikrishna <cahyadikrishna.ck@gmail.com>
2024-01-07 16:27:54 +0800

    style: adjust favicon and client book detail UI

--- src/pages/client/book-detail.jsx
+++ src/pages/client/book-detail.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button, Flex, Title, Image, Text, Badge } from "@mantine/core";
+import { Button, Flex, Title, Image, Text, Badge, Paper } from "@mantine/core";
 import { IconArrowBack } from "@tabler/icons-react";
 
 export default function PageBookDetail() {
@@ -9,7 +9,8 @@ export default function PageBookDetail() {
         direction="row"
         align="center"
         justify="space-between"
-        mb="md"
+        mb="m
+        d"
         mt={30}
       >
         <Title order={2} c="blue.5">
@@ -26,36 +27,42 @@ export default function PageBookDetail() {
         </Button>
       </Flex>
 
-      <Flex align="center" gap="xl">
-        <Image
-          radius="md"
-          miw={{ base: "100%", sm: 500 }}
-          h={{ base: 300, sm: 600 }}
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
-          alt="Book img"
-        />
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <Flex align="center" gap="xl">
+          <Image
+            radius="md"
+            miw={{ base: "100%", sm: 500 }}
+            h={{ base: 300, sm: 600 }}
+            src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-7.png"
+            alt="Book img"
+          />
 
-        <Flex direction="column" gap="xs">
-          <Flex direction="column" gap="xs" style={{ width: 500 }}>
-            <Badge color="blue">Philosophy</Badge>
+          <Flex direction="column" gap="xs">
+            <Flex direction="column" gap="xs" style={{ width: 500 }}>
+              <Badge color="blue">Philosophy</Badge>
 
-            <Title order={1}>Sang Alkemis</Title>
+              <Title order={1}>Sang Alkemis dan Pengembala Domba</Title>
 
-            <Text size="xl" fs="italic">
-              Pages: 100
-            </Text>
+              <Text size="xl" fs="italic">
+                Pages: 100
+              </Text>
 
-            <Title order={2} c="pink">
-              Rp 500.000
-            </Title>
+              <Title order={2} c="pink">
+                Rp 500.000
+              </Title>
 
-            <Text size="md">
-              Simply dummy text of the printing and typesetting industry. Lorem
-              Ipsum has been the industrys standard dummy.
-            </Text>
+              <Text size="md">
+                Simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industrys standard dummy. Lorem ipsum
+                dolor, sit amet consectetur adipisicing elit. Officiis
+                similique, adipisci aspernatur minus quo architecto quas.
+                Pariatur tempora earum rem doloribus dolor iure voluptatibus
+                animi, perspiciatis alias ut suscipit id?
+              </Text>
+            </Flex>
           </Flex>
         </Flex>
-      </Flex>
+      </Paper>
     </>
   );
 }
